6aec3ea7d238da231cd40bae10ee7cfb8f192f87
Jeishod <jeishod@gmail.com>
2022-03-18 21:46:22 +0200

    :art: format code

--- server/urls.py
+++ server/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles import views
+from django.urls import path, re_path
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    re_path(r"^static/(?P<path>.*)$", views.serve),
 ]
